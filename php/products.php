c2c7ee07da584299a298290338c66d7d73ea2775
Kaiser Mittenburg <k009m983@ku.edu>
2019-05-06 16:32:58 -0500

    Pulling and showing real products

---
+++ php/products.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "mysql.eecs.ku.edu";
+$username = "kmittenburg";
+$password = "P@\$\$word123";
+$dbname = "kmittenburg";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM PRODUCT ORDER BY PRODUCT_TYPE";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $rows = array();
+    while($r = mysqli_fetch_assoc($result)) {
+      $rows[] = $r;
+    }
+    echo json_encode($rows);
+  }else {
+    trigger_error('Invalid query: ' . $conn->error);
+    echo "0 results";
+}
+$conn->close();
+?>
