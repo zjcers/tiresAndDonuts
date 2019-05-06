cd9cd79d2d602d6d591b7f9208d78ab5e3339fde
Kaiser Mittenburg <k009m983@ku.edu>
2019-05-06 18:58:45 -0500

    Can update sku in invoice edit
    Saving still doesn't work

---
+++ php/sku.php
@@ -0,0 +1,29 @@
+<?php
+$sku = $_GET['sku'];
+$servername = "mysql.eecs.ku.edu";
+$username = "kmittenburg";
+$password = "P@\$\$word123";
+$dbname = "kmittenburg";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM PRODUCT WHERE SKU = '$sku'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $rows = array();
+    while($r = mysqli_fetch_assoc($result)) {
+      $rows[] = $r;
+    }
+    echo json_encode($rows);
+  }else {
+    trigger_error('Invalid query: ' . $conn->error);
+    echo "0 results";
+}
+$conn->close();
+?>
