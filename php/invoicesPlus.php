b3494a82ae5c3a309a8862b65e52fc4e8d78ca7d
Kaiser Mittenburg <k009m983@ku.edu>
2019-05-06 18:26:02 -0500

    View/Edit Specific Invoices Started

---
+++ php/invoicesPlus.php
@@ -0,0 +1,35 @@
+<?php
+$orderId = $_GET['orderId'];
+$servername = "mysql.eecs.ku.edu";
+$username = "kmittenburg";
+$password = "P@\$\$word123";
+$dbname = "kmittenburg";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT CUSTOMER.FIRST_NAME, CUSTOMER.LAST_NAME, PRODUCT.SKU, PRODUCT.NAME,
+PRODUCT.PRICE, SOLDITEMS.AMOUNT, PURCHASE.AMOUNT AS TOTAL
+FROM PURCHASE, SOLDITEMS, CUSTOMER, PRODUCT
+WHERE PURCHASE.PURCHASE_ID = '$orderId'
+AND CUSTOMER.CUSTOMER_ID = PURCHASE.CUSTOMER_ID
+AND PURCHASE.PURCHASE_ID = SOLDITEMS.PURCHASE_ID
+AND SOLDITEMS.SKU = PRODUCT.SKU";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $rows = array();
+    while($r = mysqli_fetch_assoc($result)) {
+      $rows[] = $r;
+    }
+    echo json_encode($rows);
+  }else {
+    trigger_error('Invalid query: ' . $conn->error);
+    echo "0 results";
+}
+$conn->close();
+?>
