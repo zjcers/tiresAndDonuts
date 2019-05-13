13e5665ad29160b76e1f380c1fcf59c474e94679
Kaiser Mittenburg <k009m983@ku.edu>
2019-05-13 16:20:44 -0500

    New Customer Done

---
+++ php/addCustomer.php
@@ -0,0 +1,25 @@
+<?php
+$fname = $_GET['fName'];
+$lname = $_GET['lName'];
+$phone = $_GET['phone'];
+
+$servername = "mysql.eecs.ku.edu";
+$username = "kmittenburg";
+$password = "P@\$\$word123";
+$dbname = "kmittenburg";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO CUSTOMER (FIRST_NAME, LAST_NAME, PHONE)
+VALUES('$fname', '$lname', '$phone')";
+$result = $conn->query($sql);
+
+echo $result;
+
+$conn->close();
+?>
