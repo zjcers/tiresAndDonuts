ae03fcab2c75d2db7a684f8586ef8e9de8eb6a75
Kaiser Mittenburg <k009m983@ku.edu>
2019-05-13 17:09:40 -0500

    New Product Done

---
+++ php/addProduct.php
@@ -0,0 +1,34 @@
+<?php
+$name = $_GET['name'];
+$inventory = $_GET['inventory'];
+$price = $_GET['price'];
+$type = $_GET['type'];
+$model = $_GET['model'];
+$manf = $_GET['manf'];
+$size = $_GET['size'];
+
+
+$servername = "mysql.eecs.ku.edu";
+$username = "kmittenburg";
+$password = "P@\$\$word123";
+$dbname = "kmittenburg";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+if($type === "Tire"){
+$sql = "INSERT INTO PRODUCT (NAME, INVENTORY, PRICE, PRODUCT_TYPE, TIRE_MODEL, TIRE_MANF, TIRE_SIZE)
+VALUES('$name', '$inventory', '$price', '$type', '$model', '$manf', '$size')";
+}
+else{
+  $sql = "INSERT INTO PRODUCT (NAME, INVENTORY, PRICE, PRODUCT_TYPE, TIRE_MODEL, TIRE_MANF, TIRE_SIZE)
+  VALUES('$name', '$inventory', '$price', '$type', NULL, NULL, NULL)";
+}
+$result = $conn->query($sql);
+echo $result;
+
+$conn->close();
+?>
